86423d18abf8ccb4b661e87206238b6a5cbb9dbd
ibrahimohid <ibrahimohid77@gmail.com>
2023-12-15 22:20:38 +0600

    I Learn Django printed

--- first_project/payments/views.py
+++ first_project/payments/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
 # Create your views here.
+def django(request):
+    return HttpResponse('I am Learning Django')

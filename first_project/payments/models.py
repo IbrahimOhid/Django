e37ff210a6e9d1d57bcf39aa27f753d12a8b3c7d
ibrahimohid <ibrahimohid77@gmail.com>
2023-12-25 14:47:13 +0600

    models added

--- first_project/payments/models.py
+++ first_project/payments/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class pay_method(models.Model):
+    pay_id = models.IntegerField()
+    pay_option = models.CharField(max_length=25)
+    pay_min = models.IntegerField()
